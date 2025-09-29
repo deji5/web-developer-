820a122d365113d13625618105f3b657d424744e
oni adedeji <onideji6@gmail.com>
2025-09-29 15:58:45 +0100

    group assignment

--- group_assignment/group_login.js
+++ group_assignment/group_login.js
@@ -1,7 +1,2 @@
-document.addEventListener('DOMContentLoaded', () => {
-      renderStudentTable();
-      document.getElementById('studentSearch').addEventListener('input', (e) => {
-        renderStudentTable(e.target.value.trim());
-      });
-    });
+
 
